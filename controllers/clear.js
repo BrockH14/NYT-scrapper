e976dc7ca1f2cae0e2e7e793faed0a6cc10b13c1
Brock Horton <brock.horton@gmail.com>
2020-06-14 12:53:52 -0400

    controllers added created corresponding routes files

---
+++ controllers/clear.js
@@ -0,0 +1,13 @@
+//Clear function
+var db = require("../models");
+module.exports = {
+  clearDB: function(req, res) {
+    db.Article.remove({})
+      .then(function() {
+        return db.Note.remove({});
+      })
+      .then(function() {
+        res.json({ ok: true });
+      });
+  }
+};
